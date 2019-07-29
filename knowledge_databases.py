0b8d14617f1824d5a38e92a60acb6af85ba1bd6d
myname21-meet <myname21@meet.mit.edu>
2019-07-29 05:16:17 -0400

    continue database lab

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,17 +8,31 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(site_url, rating, topic, title):
+	new_article=Knowledge(site_url=site_url, rating=rating, topic=topic, title=title)
+	session.add(new_article)
+	session.commit()
+#add_article("https://en.wikipedia.org/wiki/Food", 7, "food", "food")
+#add_article("https://en.wikipedia.org/wiki/Animal", 7, "animal", "animal")
+#add_article("https://en.wikipedia.org/wiki/Sport", 7, "sport", "sport")
 
 def query_all_articles():
-	pass
+	articles = session.query(Knowledge).all()
+	return articles
+all_articles = query_all_articles()
+# print(all_articles)
+def query_article_by_topic(food):
+	food_article = session.query(Knowledge).filter_by(
+       topic=food).first()
+	return food_article
+query_food = query_article_by_topic("food")
+print(query_food)
 
-def query_article_by_topic():
-	pass
-
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(sport):
+	session.query(Knowledge).filter_by(topic=sport).delete()
+	session.commit()
+delete_sport = delete_article_by_topic("sport")
+print (all_articles)
 
 def delete_all_articles():
 	pass
--- knowledge_model.py
+++ knowledge_model.py
@@ -23,12 +23,14 @@ class Knowledge(Base):
             	"Go to the wikipedia article called: {} \n"
             	"We gave this article a rating of: {} out of 10 \n"
 				"You can visit the article here: {} \n"
-				"Unfortunately, this article does not have a better rating. Maybe, this is an article that should be replaced soon!").format(
+				"Unfortunately, this article does not have a better rating. Maybe this is an article that should be replaced soon!").format(
                 	self.topic, self.title, self.rating, self.site_url)
-food=Knowledge(site_url="https://en.wikipedia.org/wiki/Food", rating=7, topic="food", title="food")
-print(food)
-animal=Knowledge(site_url="https://en.wikipedia.org/wiki/Animal", rating=6, topic="animals", title="animal")
-print(animal)
+# food=Knowledge(site_url="https://en.wikipedia.org/wiki/Food", rating=7, topic="food", title="food")
+# print(food)
+# animal=Knowledge(site_url="https://en.wikipedia.org/wiki/Animal", rating=6, topic="animals", title="animal")
+# print(animal)
+# sport=Knowledge(site_url="https://en.wikipedia.org/wiki/Sport", rating=9, topic="sports", title="sport")
+# print(sport)
 
 	# Create a table with 4 columns
 	# The first column will be the primary key
